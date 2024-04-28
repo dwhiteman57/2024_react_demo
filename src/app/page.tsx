bf32c0dbed67a1d7483e7c17b77fd1af04a7c185
dwhiteman57 <daniel.whiteman1@gmail.com>
2024-04-28 13:33:13 -0500

    Updates to database call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,19 @@ import Link from "next/link";
 import {db} from "~/server/db";
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-    "https://utfs.io/f/6cbdee13-ae68-46f5-a3e0-52ea6ec084f4-1xcy7.jpg",
-    "https://utfs.io/f/a8f324e1-b3e0-4ef4-8f78-9c1790a06cde-1xcy6.jpg"
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-    id: index + 1,
-    url
-}));
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, {desc}) => desc(model.id)
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post, index) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-
-        {mockImages.map((image, index) => (
-          <div key={image.id + index} className="w-48">
+        {images.map((image, index) => (
+          <div key={image.id + index} className="w-48 flex flex-col">
               <img src={image.url} alt="My image"/>
+              <div>{image.name}</div>
           </div>
         ))}
       </div>
