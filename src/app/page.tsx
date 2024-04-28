1718d18da854a0e1e4be83dbb6f703b2ab6d9765
dwhiteman57 <daniel.whiteman1@gmail.com>
2024-04-28 14:22:52 -0500

    Add auth for home route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,37 @@
 import Link from "next/link";
 import {db} from "~/server/db";
+import {SignedOut, SignedIn} from "@clerk/nextjs";
 export const dynamic = "force-dynamic";
 
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, {desc}) => desc(model.id)
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image, index) => (
+        <div key={image.id + index} className="w-48 flex flex-col">
+          <img src={image.url} alt="My image"/>
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image, index) => (
-          <div key={image.id + index} className="w-48 flex flex-col">
-              <img src={image.url} alt="My image"/>
-              <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center">Please Sign In....</div>
+      </SignedOut>
+
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
