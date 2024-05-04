c6f42ddc3aaae16384238a0ea10d1e949f803ba0
dwhiteman57 <daniel.whiteman1@gmail.com>
2024-05-04 08:21:36 -0500

    Updates to layout

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -9,6 +9,8 @@ export default function PhotoPage({
   if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
 
   return (
-    <FullPageImageView id={idAsNumber} />
+    <div className="h-full">
+      <FullPageImageView id={idAsNumber} />
+    </div>
   );
 }
